4449f3ff3afa835d355985a93d7f7297cf5d1f13
minjaef1003 <minjaef1003@gmail.com>
2018-05-26 21:54:30 +0900

    ios

--- UI_CSharp/IOSystem.cs
+++ UI_CSharp/IOSystem.cs
@@ -1,33 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Forms;
 
 namespace midas_challenge
 {
-    using Coordinate = KeyValuePair<int, int>;
-
     partial class Form_Main
     {
-       
-        public static void Write(List<Room> rooms)
+        public static Tuple<List<Room>, List<Furniture>> Read()
         {
-            StreamWriter sw = new StreamWriter("output.txt");
-            foreach (Room room in rooms) 
-            {
-                foreach (Wall wall in room.walls)
-                {
-                    foreach (Coordinate coord in wall.coords)
-                    {
+            List<Room> rooms;
+            List<Furniture> furnitures;
 
+            int roomCount = 0, furnitureCount = 0;
+            string line;
+            StreamReader sr = new StreamReader("test.txt");
+
+            if ((line = sr.ReadLine()) != null) {
+                roomCount = Int32.Parse(line);
+            }
+
+            rooms = new List<Room>(roomCount);
+
+            for (int i = 0; i < roomCount; i++)
+            {
+                while (((line = sr.ReadLine()) != null))
+                {
+                    string[] str = line.Split(' ', ',');
+                    if (str[0].Equals("Wall"))
+                    {
+                        rooms[i].walls.Add(new Wall(new Point(Int32.Parse(str[1]), Int32.Parse(str[2])), new Point(Int32.Parse(str[3]), Int32.Parse(str[4]))));
+                    }
+                    else if (str[0].Equals("Door"))
+                    {
+                        rooms[i].doors.Add(new Wall(new Point(Int32.Parse(str[1]), Int32.Parse(str[2])), new Point(Int32.Parse(str[3]), Int32.Parse(str[4]))));
+                    }
+                    else if (str[0].Equals("Window"))
+                    {
+                        rooms[i].windows.Add(new Wall(new Point(Int32.Parse(str[1]), Int32.Parse(str[2])), new Point(Int32.Parse(str[3]), Int32.Parse(str[4]))));
+                    }
+                    else
+                    {
+                        break;
                     }
                 }
-
             }
+            if ((line = sr.ReadLine()) != null)
+            {
+                furnitureCount = Int32.Parse(line);
+            }
+
+            furnitures = new List<Furniture>(furnitureCount);
+
+            for (int i = 0; i < furnitureCount; i++)
+            {
+                if (((line = sr.ReadLine()) != null))
+                {
+                    string[] str = line.Split(' ', ',');
+
+                    furnitures[i].name = str[1];
+                    furnitures[i].type = str[2];
+
+                    str = "Funiture " + funiture.name.ToString() + " " + furnitures.type.ToSting() + " " + funiture.imgSize.X + " " + funiture.imgSize.Y + " " + funiture.imgSize.Width + " " + funiture.imgSize.Height;
+                }
+            }
+
+            Tuple<List<Room>, List<Furniture>> result;
+
+            return result;
+        }
+        public static void Write(List<Room> rooms, List<Furniture> furnitures)
+        {
+            StreamWriter sw = new StreamWriter("test.txt");
+            string str;
+            int i = 0;
+            sw.WriteLine("Rooms count: " + rooms.Count());
+            foreach (Room room in rooms)
+            {
+                i++;
+                str = "Room " + i.ToString();
+                foreach (Wall wall in room.walls)
+                {
+                    str = "Wall " + wall.StartPoint.ToString() + " " + wall.EndPoint.ToString();
+                    sw.WriteLine(str);
+                }
+                foreach (Line door in room.doors)
+                {
+                    str = "Door " + door.StartPoint.ToString() + " " + door.EndPoint.ToString();
+                    sw.WriteLine(str);
+                }
+                foreach (Line window in room.windows)
+                {
+                    str = "Window " + window.StartPoint.ToString() + " " + window.EndPoint.ToString();
+                    sw.WriteLine(str);
+                }
+                sw.WriteLine("Room " + i.ToString() + "finish");
+            }
+            sw.WriteLine("Furniture count: " + furnitures.Count());
+            foreach (Furniture funiture in furnitures)
+            {
+                str = "Funiture " + funiture.name.ToString() + " " + furnitures.type.ToSting() + " " + funiture.imgSize.X + " " + funiture.imgSize.Y + " " + funiture.imgSize.Width + " " + funiture.imgSize.Height;
+                sw.WriteLine(str);
+            }
+            sw.Close();
         }
     }
 }
