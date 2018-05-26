d499b6ec148c066a2c157ba8c4266e3624aa07f7
minjaef1003 <minjaef1003@gmail.com>
2018-05-26 17:58:30 +0900

    IOSystem add

---
+++ UI_CSharp/IOSystem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace midas_challenge
+{
+    using Coordinate = KeyValuePair<int, int>;
+
+    partial class Form_Main
+    {
+       
+        public static void Write(List<Room> rooms)
+        {
+            StreamWriter sw = new StreamWriter("output.txt");
+            foreach (Room room in rooms) 
+            {
+                foreach (Wall wall in room.walls)
+                {
+                    foreach (Coordinate coord in wall.coords)
+                    {
+
+                    }
+                }
+
+            }
+        }
+    }
+}
