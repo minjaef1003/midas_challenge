34d95a0638da5c7ed833e3dbf3c291516254cc33
Dongmin Kim <thmlion24@gmail.com>
2018-05-26 17:29:44 +0900

    Geometry

--- UI_CSharp/Geometry.cs
+++ UI_CSharp/Geometry.cs
@@ -6,23 +6,37 @@ using System.Threading.Tasks;
 
 namespace midas_challenge
 {
-    namespace Geometry
-    {
-        /*
-        using Coordinate = KeyValuePair<int, int>;
-        class Computation
-        {
-            static public bool IsSame(Coordinate c1, Coordinate c2)
-            {
-                return false;
-            }
+    using Coordinate = KeyValuePair<int, int>;
 
-            public static double EuclideanDist(KeyValuePair<int, int> coord1, KeyValuePair<int, int> coord2)
-            {
-                return Math.Sqrt(Math.Pow((double)(coord1.Key - coord2.Key), 2.0) + Math.Pow((double)(coord2.Value - coord1.Value), 2.0));
-            }
-        }
-        */
+    public class Wall
+    {
+        private List<Coordinate> coords;
     }
 
+    public struct Room
+    {
+        public List<Wall> walls;
+        public List<KeyValuePair<Coordinate, Coordinate>> doors;
+    }
+
+    public class RoomManager
+    {
+        private List<Room> rooms;
+
+        public void writeFile()
+        {
+            IOSystem.write(rooms);
+        }
+    }
+
+    public class RoomMaker
+    {
+        private RoomManager room_manager;
+        private Room curr_room;
+        public int pushVertex(Coordinate cd)
+        {
+
+            return 0;
+        }
+    }
 }
